4eb6e51fad0b10c89729fc5ee3c2d734f60c620c
Kai Gao <73616322+kaigao-lanl@users.noreply.github.com>
2026-08-24 23:57:18 -0600

    Bug fix
    - Add branches to avoid integer overflow.

--- src/template_random_permute.f90
+++ src/template_random_permute.f90
@@ -1,15 +1,15 @@
 !
 ! © 2024. Triad National Security, LLC. All rights reserved.
 !
-! This program was produced under U.S. Government contract 89233218CNA000001 
-! for Los Alamos National Laboratory (LANL), which is operated by 
-! Triad National Security, LLC for the U.S. Department of Energy/National Nuclear 
-! Security Administration. All rights in the program are reserved by 
-! Triad National Security, LLC, and the U.S. Department of Energy/National 
-! Nuclear Security Administration. The Government is granted for itself and 
-! others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide 
-! license in this material to reproduce, prepare. derivative works, 
-! distribute copies to the public, perform publicly and display publicly, 
+! This program was produced under U.S. Government contract 89233218CNA000001
+! for Los Alamos National Laboratory (LANL), which is operated by
+! Triad National Security, LLC for the U.S. Department of Energy/National Nuclear
+! Security Administration. All rights in the program are reserved by
+! Triad National Security, LLC, and the U.S. Department of Energy/National
+! Nuclear Security Administration. The Government is granted for itself and
+! others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+! license in this material to reproduce, prepare. derivative works,
+! distribute copies to the public, perform publicly and display publicly,
 ! and to permit others to do so.
 !
 ! Author:
@@ -40,10 +40,18 @@ function random_permute_(w, seed) result(wr)
     end if
 
     if (present(seed)) then
-        do i = 1, n - 1
-            j = i + irand(range=[0, n - i], seed=seed*i)
-            call swap(wr(i), wr(j))
-        end do
+        if (seed < 0) then
+            do i = 1, n - 1
+                j = i + irand(range=[0, n - i])
+                call swap(wr(i), wr(j))
+            end do
+        else
+            do i = 1, n - 1
+                ! This is to avoid overflow
+                j = i + irand(range=[0, n - i], seed=int(modulo(int(seed, 8)*i, 2147483648_8)))
+                call swap(wr(i), wr(j))
+            end do
+        end if
     else
         do i = 1, n - 1
             j = i + irand(range=[0, n - i])
